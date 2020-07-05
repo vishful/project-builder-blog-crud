b1a8763ffa5ed447480cf7e3893561ef6874c009
Vishnu <vishnurpillai9914@gmail.com>
2020-07-06 00:21:35 +0530

    FSJ0520A101

--- src/service/CRUDOperations.java
+++ src/service/CRUDOperations.java
@@ -15,11 +15,24 @@ public class CRUDOperations {
 	}
 	
 	public List<Blog> viewBlog(Blog blog) throws IOException{
-		return null;
+		for(int i=0;i<list.size();i++) {
+			System.out.println(list.get(i).getBlogTitle()+" "+list.get(i).getBlogDescription());
+		}
+		
+		
+		return list;
 		
 	}
-	public List<Blog> updateBlog(Blog blog) throws IOException{
-		return null;
+	public List<String> updateBlog(Blog blog) throws IOException{
+		List<String> newlist = new ArrayList<String>();
+		
+		list.sort((Blog s1, Blog s2)->s1.getBlogTitle().compareTo(s2.getBlogTitle()));
+		
+		for(int i=0;i<list.size();i++) {
+			newlist.add(list.get(i).getBlogTitle());
+		}
+		
+		return newlist;
 	
 	}
 	public List<Blog> deleteBlog(Blog blog) throws IOException{
